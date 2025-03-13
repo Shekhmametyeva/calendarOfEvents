3b6fee9939179c98cea2d09c49a9b89d5eabe20f
Shekhmametyeva <alina-r-s98@outlook.com>
2025-03-13 18:23:59 +0400

    feat: add maket

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import './App.css';
+import Main from './pages/Main';
+// import React from 'react';
 
 function App() {
   return (
-    <div className='Wrapper'>
+    <div className='wrapper'>
+      <header className='header'>
+      </header>
+      <main className='main'>
+        <Main/>
+      </main>
+      <footer className='footer'>
+        <p>create by Alina</p>
+        <p>2025</p>
+      </footer>
     </div>
   );
 }
