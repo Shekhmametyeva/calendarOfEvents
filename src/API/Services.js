ffe739ab3797857389d87008494b1ac6790bd9b7
Shekhmametyeva <alina-r-s98@outlook.com>
2025-03-23 16:40:28 +0400

    refactor: correct port api

--- src/API/Services.js
+++ src/API/Services.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const getUser = async () => {
-    const response = await axios.get("http://localhost:8080/api/v1/users", {params: {id: 1}});
+    const response = await axios.get("http://localhost:8085/api/v1/users/23e3cef7-bc7d-4776-a6a5-dce8024f17e4");
     console.log(response.data);
     return response.data;
 }
